03e1d9ffadd586f198b33f6a259958a5625405ee
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2024-04-18 12:45:00 +0000

    #11 - Refatorando a busca pelo middleware no UserController
    Está apenas mais elegante:
    $this->middleware('jwtMiddleware')->except('store');
    Mas o objetivo é chamar o middleware e criar sua rota livre (except para user.store) já na leitura do route/api.php e não nos controladores de uma a um. OU:
    colocar essa escolha no prórpio middleware, fazendo ele ser chamado TODAS  as vezes mas, aplicando um filtro.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
   public function __construct()
   {
-    $this->middleware('jwtMiddleware', ['except' => ['store']]);
+    $this->middleware('jwtMiddleware')->except('store');
     
   }
   /**
